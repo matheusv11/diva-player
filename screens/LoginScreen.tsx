b00774c3ceb56f40d01cff6dcc4f9b3ba96fc145
matheusv11 <mathsales360@gmail.com>
2022-06-10 01:11:41 -0300

    Tela de login

--- screens/LoginScreen.tsx
+++ screens/LoginScreen.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, TextInput, TouchableOpacity } from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
@@ -7,9 +7,14 @@ import { RootTabScreenProps } from '../types';
 export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+      <TextInput style={styles.input} placeholder='Email' keyboardType='email-address'/>
+      <TextInput style={styles.input} placeholder='Senha' keyboardType='visible-password'/>
+      <TouchableOpacity style={styles.loginButton}>
+        <Text style={styles.title}> ENTRAR </Text>
+      </TouchableOpacity>
+      {/* <Text style={styles.title}>Login</Text> */}
+      {/* <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" /> */}
+      {/* <EditScreenInfo path="/screens/TabOneScreen.tsx" /> */}
     </View>
   );
 }
@@ -19,6 +24,23 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  input: {
+    height: 45,
+    marginBottom: 14,
+    bottom: 40,
+    borderRadius: 8,
+    paddingLeft: 12,
+    width: 340,
+    backgroundColor: 'rgba(0, 0, 0, 0.1)',
+  },
+  loginButton: {
+    backgroundColor: 'green',
+    width: 120,
+    height: 45,
+    borderRadius: 6,
+    justifyContent: 'center',
+    alignItems: 'center'
   },
   title: {
     fontSize: 20,
