b410e4b2f42aa9854d4939dae9c071533e681425
matheusv11 <mathsales360@gmail.com>
2022-06-12 00:28:51 -0300

    Apenas imagens

--- screens/RegisterScreen.tsx
+++ screens/RegisterScreen.tsx
@@ -13,7 +13,7 @@ export default function RegisterScreen({ navigation }: RootTabScreenProps<'Regis
   const pickProfile = async () => {
     // No permissions request is necessary for launching the image library
     const result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
       allowsEditing: true,
       aspect: [4, 3],
       quality: 1,
