bbdaef168138f2ce63a0e91348d0bc17c632777c
matheus.silva <matheus.silva@icolabora.com>
2022-06-21 14:19:40 -0300

    Themed Input

--- components/Themed.tsx
+++ components/Themed.tsx
@@ -3,7 +3,7 @@
  * https://docs.expo.io/guides/color-schemes/
  */
 
-import { Text as DefaultText, View as DefaultView } from 'react-native';
+import { Text as DefaultText, View as DefaultView, TextInput as DefaultTextInput } from 'react-native';
 
 import Colors from '../constants/Colors';
 import useColorScheme from '../hooks/useColorScheme';
@@ -29,6 +29,7 @@ type ThemeProps = {
 
 export type TextProps = ThemeProps & DefaultText['props'];
 export type ViewProps = ThemeProps & DefaultView['props'];
+export type InputProps = ThemeProps & DefaultTextInput['props'];
 
 export function Text(props: TextProps) {
   const { style, lightColor, darkColor, ...otherProps } = props;
@@ -42,4 +43,12 @@ export function View(props: ViewProps) {
   const backgroundColor = useThemeColor({ light: lightColor, dark: darkColor }, 'background');
 
   return <DefaultView style={[{ backgroundColor }, style]} {...otherProps} />;
+}
+
+export function Input(props: InputProps) {
+  const { style, lightColor, darkColor, ...otherProps } = props;
+  const backgroundColor = useThemeColor({ light: lightColor, dark: darkColor }, 'input');
+  const color = useThemeColor({ light: lightColor, dark: darkColor }, 'inputText'); // TODO AUTO, LOGO NÃO HÁ PROPS PRA MUDAR ISSO
+
+  return <DefaultTextInput placeholderTextColor={ color } style={[{ backgroundColor, color }, style]} {...otherProps} />;
 }
