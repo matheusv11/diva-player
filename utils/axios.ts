409a284c53c898e4293312e234502927d0e71a76
matheus.silva <matheus.silva@icolabora.com>
2022-06-23 17:26:00 -0300

    url provisoria

--- utils/axios.ts
+++ utils/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios'; // DEIXAR AXIOS GLOBAL
 
 const axiosConfig = axios.create({
     // baseURL: process.env.API_URL || 'http://localhost:3030'
-    baseURL: process.env.API_URL || 'http://192.168.1.248:3030'
+    baseURL: process.env.API_URL || 'http://192.168.1.11:3030'
 });
 
 export default axiosConfig
