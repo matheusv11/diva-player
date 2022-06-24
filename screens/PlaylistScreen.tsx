cb8835f34967d9376ce4165ef4d7c6334d3858c0
matheus.silva <matheus.silva@icolabora.com>
2022-06-23 21:01:45 -0300

    Modal playlist

--- screens/PlaylistScreen.tsx
+++ screens/PlaylistScreen.tsx
@@ -1,7 +1,7 @@
 import { useContext, useEffect, useState } from 'react';
-import { StyleSheet, TouchableOpacity, Image, ScrollView } from 'react-native';
+import { StyleSheet, TouchableOpacity, Image, ScrollView, Modal } from 'react-native';
 
-import { Text, View, Icon} from '../components/Themed';
+import { Text, View, Icon, Input, Button } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
 import { AuthContext } from '../components/AuthProvider';
 import axios from '../utils/axios';
@@ -10,6 +10,8 @@ export default function PlaylistScreen({ navigation }: RootTabScreenProps<'Home'
 
   const { token } = useContext(AuthContext);
   const [playlist, setPlaylist] = useState([]);
+  const [modalVisible, setModalVisible] = useState(false);
+  const [newPlaylist, setNewPlaylist] = useState("");
 
   useEffect(() => {
     axios.get("/all-playlists", {
@@ -25,12 +27,66 @@ export default function PlaylistScreen({ navigation }: RootTabScreenProps<'Home'
     })
   }, []);
 
+  const createPlaylist = () => {
+    axios.post("/playlist", { nome: newPlaylist}, {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    })
+    .then(res => {
+      setPlaylist([...playlist, res.data.created]);
+      setNewPlaylist("");
+      setModalVisible(false)
+    })
+    .catch(err => {
+      alert(err)
+    })
+  }
+
+  const deletePlaylist = (playlistId: number) => {
+    axios.delete(`/playlist/${playlistId}`, {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    })
+    .then(res => {
+      const filter = playlist.filter(play => play.id !== playlistId)
+      setPlaylist(filter)
+    })
+    .catch(err => {
+      alert(err)
+    })
+  }
+
   return (
     <View style={styles.container}>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          setModalVisible(false);
+        }}
+      >
+        <View style={styles.centeredModal}>
+          <View style={styles.modalView}>
+            <Input value={newPlaylist} onChangeText={text => setNewPlaylist(text)} style={styles.input} placeholder='Playlist' keyboardType='default'/>
+            <View style={styles.modalButtons}>
+              <Button onPress={createPlaylist} style={styles.closeButton}>
+                <Text style={styles.closeButtonText}> Criar </Text>
+              </Button>
+              <Button onPress={() => setModalVisible(false)} style={styles.closeButton}>
+                <Text style={styles.closeButtonText}> Fechar </Text>
+              </Button>
+            </View>
+            
+          </View>
+        </View>
+      </Modal>
 
       <View style={styles.playlistTop}>
         <Text style={styles.playlistTopText}>Playlist</Text>
-        <TouchableOpacity>
+        <TouchableOpacity onPress={() => setModalVisible(true)}>
           <Icon
             name="plus-circle"
             size={35}
@@ -40,10 +96,17 @@ export default function PlaylistScreen({ navigation }: RootTabScreenProps<'Home'
       <ScrollView>
       {playlist.map(play => (
         <View style={styles.musicList} key={play.id}>
-
-            <Image source={{ uri: play.musica_playlist[0].thumbnail }} style={styles.musicImage}/>
+          <View style={styles.musicListLeft}>
+            <Image source={{ uri: play.musica_playlist[0]?.thumbnail || 'https://reactjs.org/logo-og.png' }} style={styles.musicImage}/>
             <Text style={styles.musicTitle}> {play.nome} </Text>
 
+          </View>
+
+          <View style={styles.musicListRight}>
+            <TouchableOpacity onPress={() => deletePlaylist(play.id)}>
+              <Icon name="trash" size={35} style={styles.musicTrash}/>
+            </TouchableOpacity>
+          </View>
         </View>
       ))}
       </ScrollView>
@@ -60,7 +123,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'space-between',
     marginTop: 20,
-    padding: 4,
+    paddingLeft: 20,
+    paddingRight: 20,
     width: "100%"
   },
   playlistTopText: {
@@ -70,8 +134,18 @@ const styles = StyleSheet.create({
   musicList: {
     flexDirection: 'row',
     width: "100%",
-    alignItems: 'center',
     marginTop: 20,
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  musicListLeft: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  musicListRight: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginRight: 16,
   },
   musicImage: {
     width: 112, 
@@ -83,5 +157,51 @@ const styles = StyleSheet.create({
     marginBottom: 6,
     fontSize: 16,
     fontWeight: "bold"
-  }
+  },
+  musicTrash: {
+    // marginHorizontal: 
+  },
+  input: {
+    height: 45,
+    borderRadius: 8,
+    paddingLeft: 12,
+    width: 340,
+  },
+  closeButton: {
+    width: 120,
+    top: 20,
+    marginLeft: 12,
+    marginRight: 12,
+    height: 45,
+    borderRadius: 6,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  modalButtons: {
+    flexDirection: 'row'
+  },
+  closeButtonText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  centeredModal: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    // backgroundColor: 'transparent',
+    backgroundColor: 'rgba(0,0,0,0.2)',
+  },
+  modalView: {
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5
+  },
 });
